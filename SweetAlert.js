29635aabff8b7eca1bf2f0930a09f6310cf05b67
Justas Brazauskas <brazauskasjustas@gmail.com>
2014-11-28 17:48:10 +0200

    Added promises to SweetAlert

--- SweetAlert.js
+++ SweetAlert.js
@@ -1,70 +1,41 @@
-/**
-@fileOverview
+(function () {
+	'use strict';
 
-@toc
+	angular.module('jutaz.ngSweetAlertPromised', []).factory('SweetAlert',
+	['$window', '$q', function ($window, $q) {
 
-*/
+		var swal = $window.swal;
 
-'use strict';
+		var self = {
+			swal: function (options) {
+				var defer = $q.defer();
+				swal(options, function (isConfirm) {
+					if (!isConfirm) {
+						defer.reject();
+					} else {
+						defer.resolve();
+					}
+				});
+				return defer.promise;
+			},
+			success: function(options) {
+				options.type = 'success';
+				return self.swal(options);
+			},
+			error: function(options) {
+				options.type = 'error';
+				return self.swal(options);
+			},
+			warning: function(options) {
+				options.type = 'warning';
+				return self.swal(options);
+			},
+			info: function(options) {
+				options.type = 'info';
+				return self.swal(options);
+			}
+		};
 
-angular.module('oitozero.ngSweetAlert', [])
-.factory('SweetAlert', [ '$timeout', '$window', function ( $timeout, $window ) {
-
-	var swal = $window.swal;
-
-	//public methods
-	var self = {
-
-		swal: function ( arg1, arg2, arg3 ) {
-			$timeout(function(){
-				if( typeof(arg2) === 'function' ) {
-					swal( arg1, function(isConfirm){
-						$timeout( function(){
-							arg2(isConfirm);
-						});
-					}, arg3 );
-				} else {
-					swal( arg1, arg2, arg3 );
-				}
-			}, 200);
-		},
-		adv: function( object ) {
-			$timeout(function() {
-				swal( object );
-			}, 200);
-		},
-		timed: function( title, message, type, time ) {
-			$timeout(function() {
-				swal( {
-					title: title,
-					text: message,
-					type: type,
-					timer: time
-				} );
-			}, 200);
-		},
-		success: function(title, message) {
-			$timeout(function(){
-				swal( title, message, 'success' );
-			}, 200);
-		},
-		error: function(title, message) {
-			$timeout(function(){
-				swal( title, message, 'error' );
-			}, 200);
-		},
-		warning: function(title, message) {
-			$timeout(function(){
-				swal( title, message, 'warning' );
-			}, 200);
-		},
-		info: function(title, message) {	
-			$timeout(function(){
-				swal( title, message, 'info' );
-			}, 200);
-		}
-	};
-	
-	return self;
-}]);
-
+		return self;
+	}]);
+})();
